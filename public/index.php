8b2afbb0da42b41f7b341144c838ddc8f304573a
TimGa <tgavryutenko@mail.ru>
2020-08-23 20:23:50 +0300

    Replace own Http Request/Respone with laminas-diactoros

--- public/index.php
+++ public/index.php
@@ -1,22 +1,22 @@
 <?php
 
-use Framework\Http\RequestFactory;
-use Framework\Http\Response;
+use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\Diactoros\Response;
 
 chdir(dirname(__DIR__));
 require_once 'vendor/autoload.php';
 
-$request = RequestFactory::fromGlobals()->withParsedBody([1,2,3]);
-
+$request = ServerRequestFactory::fromGlobals();
 $name = $request->getQueryParams()['name'] ?? "Guest";
 $body = "Hello, $name!" . "<br>";
 
 $response = (new Response())
         ->withHeader('X-h1', 'h1')
         ->withHeader('X-h2', 'h2')
-        ->withHeader('X-h1', ['h1', 'additional-h1'])
-        ->withStatus(200)
-        ->withBody($body);
+        ->withAddedHeader('X-h1', 'added-h1')
+        ->withStatus(222, 'tim-phrase');
+
+$response->getBody()->write($body);
 
 header('HTTP/1.1 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
 foreach ($response->getHeaders() as $name => $values) {
